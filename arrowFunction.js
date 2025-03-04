ee8f3f37eb50a8e842af17287e50ae8d2ee510be
its-MAHADi <mhkhandakar57@gmail.com>
2025-03-05 02:00:27 +0600

    spread practice

--- arrowFunction.js
+++ arrowFunction.js
@@ -1,20 +1,20 @@
-const text = () => console.log("hello empty");
-text()
+// const text = () => console.log("hello empty");
+// text()
 
 
 
-const squre = x => x * x; 
-console.log(squre(20))
+// const squre = x => x * x; 
+// console.log(squre(20))
 
 
 
 
-const Squre = y => {
-    return y * y;
-}
-console.log(Squre (10));
+// const Squre = y => {
+//     return y * y;
+// }
+// console.log(Squre (10));
 
 
-document.getElementById("title").addEventListener("click",() => {
-   console.log("hello")
-})
+// document.getElementById("title").addEventListener("click",() => {
+//    console.log("hello")
+// })
---
+++ spread.js
@@ -0,0 +1,12 @@
+const friends = ["mahadi","hasan","mahadi"];
+// console.log(...friends);
+
+
+const Friends = ["masud","maruf","masum","mahmud"]
+const newArray = [...Friends,"mahadi"]
+// console.log(newArray)
+
+
+const numbers = [5,10,15,20,35,67,88,99,123,444,555,666,667];
+console.log(Math.max(...numbers))    //output : NaN  (Not a number)  
+// erpor spread (...) deoyar por...output : 667 big number
